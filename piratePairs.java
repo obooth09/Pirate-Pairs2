d01ea452ce0f70b1b66533ca057b376acf870431
obooth09 <obooth@brooksschool.org>
2026-02-10 09:47:15 -0500

    got deck to work in array

--- piratePairs.java
+++ piratePairs.java
@@ -1,17 +1,17 @@
-import javax.smartcardio.Card;
+import java.util.Arrays;
 
-public class piratePairs {
+public class PiratePairs {
     public static void main (String [] args) {
-        Card [] cards = new Card[55];
+       int [] cards = new int [55];
         int value = 0;
 
         for (int i = 1; i <= 10; i++){
             for (int j = 0; j < i; j++){
-                cards[value++] = new Card(j);
+                cards[value] = i;
+                value ++;
             }
         }
-        System.out.println(value);
-
-        //when you draw a card make sure to take it away from the array
+        System.out.println(Arrays.toString(cards));
     }
+    
 }
