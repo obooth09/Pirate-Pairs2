4db40e2bb7f9a4ca542f20beef6333f1ffabf292
obooth09 <obooth@brooksschool.org>
2026-02-11 10:12:54 -0500

    removed a place from the draw card array when its called

--- piratePairs.java
+++ piratePairs.java
@@ -13,6 +13,7 @@ public class PiratePairs {
         }
         //System.out.println(Arrays.toString(cards));
         shuffleDeck(cards);
+        
     }
 
     public static void shuffleDeck(int [] cards){  
@@ -29,8 +30,11 @@ public class PiratePairs {
     }
     public static int drawCard(int [] cards){
         int drawnCard = cards [0];
+        for (int i = 0; i < cards.length -  1; i++){
+            cards[i] = cards[i + 1];
+        }
+        cards[cards.length - 1] = 0;
         return drawnCard;
-        //find out how to remove the spot from the array
     }
 
 }
