1844669ff2872b6a41a97a408a4b330187408be6
obooth09 <obooth@brooksschool.org>
2026-02-15 19:55:58 -0500

    added everything into a loop

--- piratePairs.java
+++ piratePairs.java
@@ -10,21 +10,34 @@ public class PiratePairs {
         players[1] = new Player();
 
         deck.shuffle();
+        for (int i = 0; i < players.length; i++){
+            players[i].addCard(deck.dealCard());
+        }
+        //deck.show();
 
         int current = 0; // current person who is playing
+        for (int i = 0; i < 10; i ++){
+            int drawn = deck.dealCard();
+            System.out.println("Player " + current + " draws a " + drawn);
 
-        int drawn = deck.dealCard();
-        System.out.println("Player " + current + " draws a " + drawn);
-
-        if (players[current].hasCard(drawn)){
-            System.out.println("PAIR. Player" + current + " loses the round");
-            players[current].clearHand(); 
+            if (players[current].hasCard(drawn)){
+                System.out.println("PAIR. Player " + current + " loses the round");
+                players[current].clearHand(); 
             //later change the score here
-        } else {
-            players[current].addCard(drawn);
+            } else {
+                players[current].addCard(drawn);
+            }
+
+            players[current].showHand();
+            //i looked this up so that it goes back to player 1
+            current = (current + 1) % players.length;
         }
 
-        players[current].showHand();
+        
+
+        
+
+        
 
         //players[0].hasPair();
 
